fb3acf797e5ad3aeeaf8f30e08d7cfa46b9bdc30
sonu kumar <sonu.kumar@furation.tech>
2023-08-16 11:47:02 +0530

    feat: space removed in update-attendance.ts

--- src/domain/attendence/usecases/update-attendance.ts
+++ src/domain/attendence/usecases/update-attendance.ts
@@ -1,5 +1,4 @@
-
-import { AttendanceEntity,AttendanceModel } from "../entities/attendence";
+import { AttendanceEntity, AttendanceModel } from "../entities/attendence";
 import { AttendanceRepository } from "../repositories/attendence-repository";
 
 export interface UpdateAdminUsecase {
@@ -20,14 +19,11 @@ export class UpdateAdmin implements UpdateAdminUsecase {
     adminId: string,
     adminData: Partial<AttendanceModel>
   ): Promise<AttendanceEntity> {
-
-
-
     const existingAdmin: AttendanceEntity | null =
       await this.AttendanceRepository.getAttendanceById(adminId);
 
     // Perform the partial update by merging adminData with existingAdmin
-    const updatedAdminData:AttendanceEntity | any = {
+    const updatedAdminData: AttendanceEntity | any = {
       ...existingAdmin,
       ...adminData,
     };
